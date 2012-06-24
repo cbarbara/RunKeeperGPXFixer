275dae319b5611453ef738f868def9dccbe4b8ed
Chris Barbara <cbarbara@gmail.com>
2012-06-24 14:35:11 -0700

    Initial commit
    if you want to export your GPX files from RunKeeper and into another app
    that takes GPX files (ex: Strava), you will need this to fix the times
    of your run/rides.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ RunKeeperGPXFixer
 =================
 
 RunKeeper's GPX export would not have the time stamps in UTC time. This simple app fixes the time stamps and writes out a new file.
+
---
+++ RunKeeperGPXFixer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace RunKeeperGPXFixer
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			if ( args.Length != 1 )
+			{
+				return;
+			}
+			string fileName = args[ 0 ];
+			StringBuilder fileContent = new StringBuilder( System.IO.File.ReadAllText( fileName ) );
+			
+			XmlDocument xdoc = new XmlDocument();
+			xdoc.LoadXml( fileContent.ToString() );
+
+			System.Xml.XmlNamespaceManager xmlnsManager = new System.Xml.XmlNamespaceManager( xdoc.NameTable );
+			xmlnsManager.AddNamespace( "gpx", "http://www.topografix.com/GPX/1/0" );
+
+			XmlNodeList timeNodes = xdoc.SelectNodes( "//gpx:time", xmlnsManager );
+			foreach ( XmlNode timeNode in timeNodes )
+			{
+				string[] split1 = timeNode.InnerText.Split( 'T' );
+				string[] splitDays = split1[ 0 ].Split( '-' );
+				string[] splitHours = split1[ 1 ].Replace( "Z", "" ).Split( ':' );
+
+				fileContent.Replace( timeNode.InnerText, new DateTime( int.Parse( splitDays[ 0 ] ), int.Parse( splitDays[ 1 ] ), int.Parse( splitDays[ 2 ] ),
+					int.Parse( splitHours[ 0 ] ), int.Parse( splitHours[ 1 ] ), int.Parse( splitHours[ 2 ] ) ).ToUniversalTime().ToString( "s" ) + "Z" );
+			}
+
+			System.IO.File.WriteAllText( fileName.Replace( ".gpx", "-fix.gpx" ), fileContent.ToString() );
+		}
+	}
+}
